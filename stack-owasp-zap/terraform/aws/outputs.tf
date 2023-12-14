1fe3ff77b4743a3db218d4f7a8de51cea091cfae
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-12-14 21:26:04 +0100

    minor fix

--- stack-owasp-zap/terraform/aws/outputs.tf
+++ stack-owasp-zap/terraform/aws/outputs.tf
@@ -3,9 +3,9 @@ output "vm_ip" {
   value       = module.zap.vm_ip
 }
 
-output "port" {
+output "zap_port" {
   description = "Port where OWASP Zed Attack Proxy (ZAP) service is exposed"
-  value       = module.zap.port
+  value       = module.zap.zap_port
 }
 
 output "vm_os_user" {
