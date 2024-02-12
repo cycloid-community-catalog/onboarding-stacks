5fbf818d40c0b0c557ef04283676ed29abeb1dba
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2024-02-12 18:19:14 +0100

    new ec2 stack

---
+++ stack-compute-aws/terraform/outputs.tf
@@ -0,0 +1,15 @@
+#
+# VPC outputs
+#
+output "vpc_id" {
+  description = "The VPC ID for the VPC"
+  value       = module.compute.vpc_id
+}
+
+#
+# Instance outputs
+#
+output "vm_ssh" {
+  description = "The SSH address to connect to the instance"
+  value       = "${module.compute.vm_os_user}@${module.compute.vm_ip}"
+}
