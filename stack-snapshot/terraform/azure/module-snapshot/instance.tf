146e9529dc60d0e34ba021f4ffe142892c7fec69
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2024-02-22 17:12:51 +0100

    fix description

--- stack-snapshot/terraform/azure/module-snapshot/instance.tf
+++ stack-snapshot/terraform/azure/module-snapshot/instance.tf
@@ -1,12 +1,11 @@
 # Create snapshot VM
 resource "azurerm_linux_virtual_machine" "snapshot" {
   name                  = "${var.customer}-${var.project}-${var.env}"
-  computer_name         = "${var.customer}-${var.project}-${var.env}"
   resource_group_name   = data.azurerm_resource_group.snapshot.name
   location              = var.azure_location
   network_interface_ids = [azurerm_network_interface.snapshot.id]
   size                  = var.vm_instance_type
-  admin_username        = var.vm_os_user
+  admin_username        = "snapshot"
 
   os_disk {
     caching              = "ReadWrite"
--- stack-snapshot/terraform/azure/module-snapshot/resource_group.tf
+++ stack-snapshot/terraform/azure/module-snapshot/resource_group.tf
@@ -1,3 +1,3 @@
-data "azurerm_resource_group" "zap" {
+data "azurerm_resource_group" "snapshot" {
   name = var.resource_group_name
 }
