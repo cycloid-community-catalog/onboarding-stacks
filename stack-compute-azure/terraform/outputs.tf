9f3dc92d5a0d28e0994b31f535156b49a0a4a284
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2024-02-22 14:34:45 +0100

    fix outputs

--- stack-compute-azure/terraform/outputs.tf
+++ stack-compute-azure/terraform/outputs.tf
@@ -3,7 +3,7 @@
 #
 output "resource_group_name" {
   description = "The name for the Resource Group"
-  value       = module.webapp.resource_group_name
+  value       = module.compute.resource_group_name
 }
 
 #
@@ -11,7 +11,7 @@ output "resource_group_name" {
 #
 output "vnet_name" {
   description = "The name for the virtual network"
-  value       = module.webapp.vnet_name
+  value       = module.compute.vnet_name
 }
 
 #
@@ -19,10 +19,10 @@ output "vnet_name" {
 #
 output "vm_ssh" {
   description = "The SSH address to connect to the instance"
-  value       = "${module.webapp.vm_os_user}@${module.webapp.vm_ip}"
+  value       = "${module.compute.vm_os_user}@${module.compute.vm_ip}"
 }
 
 output "url" {
   description = "The URL of the wepapp"
-  value       = "http://${module.webapp.vm_ip}"
+  value       = "http://${module.compute.vm_ip}"
 }
