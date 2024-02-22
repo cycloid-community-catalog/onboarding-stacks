e31f09089ef0a3fde1b47563d87582ea9439121f
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2024-02-22 18:15:53 +0100

    fix vars

--- stack-snapshot/terraform/azure/main.tf
+++ stack-snapshot/terraform/azure/main.tf
@@ -14,14 +14,6 @@ module "snapshot" {
     monitoring_discovery = false
   }
 
-  #. key_pair_public: ""
-  #+ The public SSH key, for SSH access to newly-created instances
-  key_pair_public = var.key_pair_public
-
-  #
-  # Snapshot
-  #
-
   #. vm_instance_type: 'Standard_DS2_v2'
   #+ Instance type for the Snapshot
   vm_instance_type = "Value injected by StackForms"
@@ -29,10 +21,6 @@ module "snapshot" {
   #. vm_disk_size: 30
   #+ Disk size for the Snapshot (Go)
   vm_disk_size = "Value injected by StackForms"
-
-  #. zap_port: 8080
-  #+ Port where Snapshot service is exposed
-  zap_port = "Value injected by StackForms"
 
   #. resource_group_name: ''
   #+ The name of the existing resource group where the resources will be deployed
