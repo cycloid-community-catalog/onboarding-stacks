332b3752496f135021c6370757e8abf91882edc0
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2024-05-16 12:41:48 +0200

    add azure env var to az login

---
+++ stack-kubernetes-azure/terraform/aks/azure-env.tf
@@ -0,0 +1,11 @@
+# set up env vars for post-installation to work via the Azure CLI
+resource "null_resource" "env-cred" {
+  provisioner "local-exec" {
+    command = "env"
+    environment = {
+      ARM_SUBSCRIPTION_ID = var.azure_cred.subscription_id
+      ARM_TENANT_ID = var.azure_cred.tenant_id
+      ARM_CLIENT_ID = var.azure_cred.client_id
+      ARM_CLIENT_SECRET = var.azure_cred.client_secret
+  }
+}
