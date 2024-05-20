d64f40b658be3090c105116f26afcac4f5f11a34
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2024-05-20 22:46:57 +0200

    fix backend ref

--- stack-vmss-azure/terraform/module-scaleset/scaleset.tf
+++ stack-vmss-azure/terraform/module-scaleset/scaleset.tf
@@ -69,7 +69,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "web" {
       name      = "internal"
       primary   = true
       subnet_id = azurerm_subnet.subnet.id
-      load_balancer_backend_address_pool_ids = azurerm_lb_backend_address_pool.web_lb_backend_address_pool.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.web_lb_backend_address_pool.id]
     }
   }
 
