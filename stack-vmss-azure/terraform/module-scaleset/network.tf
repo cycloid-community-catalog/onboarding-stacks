dd7174b83744d051b35f97673c0093f1c5e5ae56
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2024-05-20 22:44:04 +0200

    fix rg

--- stack-vmss-azure/terraform/module-scaleset/network.tf
+++ stack-vmss-azure/terraform/module-scaleset/network.tf
@@ -13,6 +13,6 @@ resource "azurerm_virtual_network" "virtual_network" {
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.project}-${var.env}"
   resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.rg.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
