214b5b7bdafe1c46657c646e9dd5fac895179720
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2024-05-21 18:10:28 +0200

    add outputs

---
+++ stack-vmss-azure/terraform/outputs.tf
@@ -0,0 +1,30 @@
+# LB Public IP
+output "web_lb_public_ip_address" {
+  description = "Web Load Balancer Public Address"
+  value = module.scaleset.web_lb_public_ip_address
+}
+
+# Load Balancer ID
+output "web_lb_id" {
+  description = "Web Load Balancer ID."
+  value = module.scaleset.web_lb_id
+}
+
+# Load Balancer Frontend IP Configuration Block
+output "web_lb_frontend_ip_configuration" {
+  description = "Web LB frontend_ip_configuration Block"
+  value = module.scaleset.web_lb_frontend_ip_configuration
+}
+
+# User password to access the VMSS nodes
+output "vmss_password" {
+  description = "User password to access the VMSS nodes"
+  value = module.scaleset.vmss_password
+  sensitive = true
+}
+
+# The Hostname of the Redis Instance
+output "redis_host" {
+  description = "The private IP address of the Redis Instance."
+  value = module.scaleset.redis_host
+}
