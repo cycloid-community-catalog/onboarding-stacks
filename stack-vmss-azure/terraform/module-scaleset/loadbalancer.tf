58dbbc4f4258970b8bdabf16235acc22add5269d
Olivier <olivier3t@gmail.com>
2024-05-27 14:48:14 +0200

    minor updates

--- stack-vmss-azure/terraform/module-scaleset/loadbalancer.tf
+++ stack-vmss-azure/terraform/module-scaleset/loadbalancer.tf
@@ -40,7 +40,7 @@ resource "azurerm_lb_backend_address_pool" "web_lb_backend_address_pool" {
 resource "azurerm_lb_probe" "web_lb_probe" {
   name            = "${var.project}-${var.env}-probe"
   loadbalancer_id = azurerm_lb.web_lb.id
-  protocol        = "TCP"
+  protocol        = "Tcp"
   port            = 8080
 }
 
