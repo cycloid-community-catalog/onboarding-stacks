ac47d88fa451b0819a7559cb0ce9212ae62ac355
Olivier <olivier3t@gmail.com>
2024-11-07 14:49:14 +0100

    update service plan

--- stack-functions-azure/terraform/module-function/service_plan.tf
+++ stack-functions-azure/terraform/module-function/service_plan.tf
@@ -1,13 +1,9 @@
-resource "azurerm_app_service_plan" "app_service_plan" {
+resource "azurerm_service_plan" "service_plan" {
   name                = "${var.customer}-${var.project}-${var.env}"
   resource_group_name = data.azurerm_resource_group.resource_group.name
   location            = var.azure_location
-  kind                = "FunctionApp"
-  reserved = true # this has to be set to true for Linux. Not related to the Premium Plan
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
+  os_type             = "Linux"
+  sku_name            = "P1v2"
 
   tags = merge(local.merged_tags, {
     Name = "${var.customer}-${var.project}-${var.env}"
