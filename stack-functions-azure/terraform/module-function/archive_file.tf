84ce8c624cbed5f8de9b3c9acd3c3ad3719f17be
Olivier <olivier3t@gmail.com>
2024-11-07 18:08:40 +0100

    minor fix

--- stack-functions-azure/terraform/module-function/archive_file.tf
+++ stack-functions-azure/terraform/module-function/archive_file.tf
@@ -2,4 +2,8 @@ data "archive_file" "function_package" {
   type = "zip"
   source_dir = "${path.module}/../../git_function/azure-vnet"
   output_path = "${path.module}/function.zip"
+  
+  depends_on = [
+    random_string.r
+  ]
 }
