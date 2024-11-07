570720f2ef741c93da836fcd1f0ce92f2b600a0e
Olivier <olivier3t@gmail.com>
2024-11-07 17:46:28 +0100

    minor fix

--- stack-functions-azure/terraform/module-function/archive_file.tf
+++ stack-functions-azure/terraform/module-function/archive_file.tf
@@ -1,5 +1,5 @@
 data "archive_file" "function_package" {
   type = "zip"
-  source_dir = "../git_function"
-  output_path = "function.zip"
+  source_dir = "${path.module}/../../git_function/azure-vnet"
+  output_path = "${path.module}/function.zip"
 }
