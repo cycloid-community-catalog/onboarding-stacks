3cbc9a04cddd7ca5b31e837b04e196af4b8e72a0
Olivier <olivier3t@gmail.com>
2024-11-07 17:37:33 +0100

    minor fix

--- stack-functions-azure/terraform/module-function/storage_account.tf
+++ stack-functions-azure/terraform/module-function/storage_account.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage_account" {
-  name = "cycloid_${var.project}_${var.env}"
+  name = "cycloid${var.project}${var.env}"
   resource_group_name = data.azurerm_resource_group.resource_group.name
   location = var.azure_location
   account_tier = "Standard"
