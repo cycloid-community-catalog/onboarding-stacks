80999ccc8194a9b1d2b66de4c0bbc86b86edfde4
olivier2t <80399237+olivier2t@users.noreply.github.com>
2024-11-09 13:03:04 +0100

    Update main.tf

--- stack-functions-azure/terraform/main.tf
+++ stack-functions-azure/terraform/main.tf
@@ -22,4 +22,8 @@ module "function" {
   #+ Azure location
   azure_location = "Value injected by StackForms"
 
+  #. git_func_path: "West Europe"
+  #+ Git repository path where the azure function is stored.
+  git_func_path = var.git_func_path
+
 }
