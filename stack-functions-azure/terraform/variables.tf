729e9f4b1ee30a3cb5de85c551e9664538e55837
olivier2t <80399237+olivier2t@users.noreply.github.com>
2024-11-09 09:24:45 +0100

    Update variables.tf

--- stack-functions-azure/terraform/variables.tf
+++ stack-functions-azure/terraform/variables.tf
@@ -7,15 +7,3 @@ variable "customer" {}
 variable "azure_cred" {
   description = "The azure credential used to deploy the infrastructure. It contains subscription_id, tenant_id, client_id, and client_secret"
 }
-
-variable "git_func_url" {
-  description = "The git repository URL where the azure function to deploy is located."
-}
-
-variable "git_func_branch" {
-  description = "The git repository branch where the azure function to deploy is located."
-}
-
-variable "git_func_path" {
-  description = "The git repository path where the azure function to deploy is located. Can be blank if the function is in the root directory."
-}
