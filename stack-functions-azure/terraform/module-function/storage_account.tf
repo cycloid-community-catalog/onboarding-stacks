36ecc97ac6ae466706dbd4ab03819d6de8d8bb5c
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2024-11-12 00:39:36 +0100

    minor fix

--- stack-functions-azure/terraform/module-function/storage_account.tf
+++ stack-functions-azure/terraform/module-function/storage_account.tf
@@ -12,46 +12,46 @@ resource "azurerm_storage_account" "storage_account" {
   })
 }
 
-resource "azurerm_storage_container" "storage_container" {
-    name = "function-releases"
-    storage_account_name = azurerm_storage_account.storage_account.name
-    container_access_type = "private"
-}
+# resource "azurerm_storage_container" "storage_container" {
+#     name = "function-releases"
+#     storage_account_name = azurerm_storage_account.storage_account.name
+#     container_access_type = "private"
+# }
 
-resource "azurerm_storage_blob" "storage_blob" {
-    name = "${var.project}${var.env}.zip"
-    storage_account_name = azurerm_storage_account.storage_account.name
-    storage_container_name = azurerm_storage_container.storage_container.name
-    type = "Block"
-    source = data.archive_file.function_package.output_path
-}
+# resource "azurerm_storage_blob" "storage_blob" {
+#     name = "${var.project}${var.env}.zip"
+#     storage_account_name = azurerm_storage_account.storage_account.name
+#     storage_container_name = azurerm_storage_container.storage_container.name
+#     type = "Block"
+#     source = data.archive_file.function_package.output_path
+# }
 
-data "azurerm_storage_account_sas" "storage_account_sas" {
-    connection_string = azurerm_storage_account.storage_account.primary_connection_string
-    https_only = true
-    start = "2014-01-01"
-    expiry = "2026-12-31"
-    resource_types {
-        object = true
-        container = false
-        service = false
-    }
-    services {
-        blob = true
-        queue = false
-        table = false
-        file = false
-    }
-    permissions {
-        read = true
-        write = false
-        delete = false
-        list = false
-        add = false
-        create = false
-        update = false
-        process = false
-        tag = false
-        filter  = false
-    }
-}
+# data "azurerm_storage_account_sas" "storage_account_sas" {
+#     connection_string = azurerm_storage_account.storage_account.primary_connection_string
+#     https_only = true
+#     start = "2014-01-01"
+#     expiry = "2026-12-31"
+#     resource_types {
+#         object = true
+#         container = false
+#         service = false
+#     }
+#     services {
+#         blob = true
+#         queue = false
+#         table = false
+#         file = false
+#     }
+#     permissions {
+#         read = true
+#         write = false
+#         delete = false
+#         list = false
+#         add = false
+#         create = false
+#         update = false
+#         process = false
+#         tag = false
+#         filter  = false
+#     }
+# }
