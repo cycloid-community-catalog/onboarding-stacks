953a5a9bdd251200d8026efd52d3c005deb993f5
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2024-11-12 00:20:54 +0100

    minor fix

--- stack-functions-azure/terraform/module-function/service_plan.tf
+++ stack-functions-azure/terraform/module-function/service_plan.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "service_plan" {
   resource_group_name = data.azurerm_resource_group.resource_group.name
   location            = var.azure_location
   os_type             = "Linux"
-  sku_name            = "Dynamic"
+  sku_name            = "Shared"
 
   tags = merge(local.merged_tags, {
     Name = "${var.customer}-${var.project}-${var.env}"
