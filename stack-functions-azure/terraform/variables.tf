76d397c14979218eb292c29d0d9932b4afba14f8
olivier2t <80399237+olivier2t@users.noreply.github.com>
2024-11-13 22:05:27 +0100

    Update variables.tf

--- stack-functions-azure/terraform/variables.tf
+++ stack-functions-azure/terraform/variables.tf
@@ -7,7 +7,3 @@ variable "customer" {}
 variable "azure_cred" {
   description = "The azure credential used to deploy the infrastructure. It contains subscription_id, tenant_id, client_id, and client_secret"
 }
-
-variable "git_func_path" {
-  description = "The git repository path where the azure function package (zip file) is stored."
-}
