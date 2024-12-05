b5dc1621aaef4dd5e72592f36f27be66c31dd355
Olivier <olivier3t@gmail.com>
2024-12-05 18:17:36 +0100

    fix  paths

--- stack-valuesref/terraform/azure/module-function/role_assignment.tf
+++ stack-valuesref/terraform/azure/module-function/role_assignment.tf
@@ -1,8 +1,5 @@
-data "azurerm_subscription" "primary" {
-}
-
 resource "azurerm_role_assignment" "role_assignment" {
-  scope                = "/subscriptions/${data.azurerm_subscription.primary.id}"
+  scope                = "/subscriptions/508f906f-b287-4882-b038-c653fe001aa0"
   role_definition_name = "Reader"
   principal_id         = azurerm_linux_function_app.linux_function_app.identity.0.principal_id
 }
