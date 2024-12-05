dde9ac621611c377db75c5124224e0a683a8d968
Olivier <olivier3t@gmail.com>
2024-12-05 18:00:06 +0100

    fix  paths

--- stack-valuesref/terraform/azure/module-function/identity.tf
+++ stack-valuesref/terraform/azure/module-function/identity.tf
@@ -1,7 +1,7 @@
 data "azurerm_subscription" "primary" {
 }
 
-data "azurerm_system_assigned_identity" "system_assigned_identity" {
+data "azurerm_user_assigned_identity" "user_assigned_identity" {
   name                = azurerm_linux_function_app.linux_function_app.identity.0.name
   resource_group_name = azurerm_linux_function_app.linux_function_app.resource_group_name
 }
@@ -9,5 +9,5 @@ data "azurerm_system_assigned_identity" "system_assigned_identity" {
 resource "azurerm_role_assignment" "role_assignment" {
   scope                = data.azurerm_subscription.primary.id
   role_definition_name = "Reader"
-  principal_id         = data.azurerm_system_assigned_identity.system_assigned_identity.principal_id
+  principal_id         = data.azurerm_user_assigned_identity.user_assigned_identity.principal_id
 }
