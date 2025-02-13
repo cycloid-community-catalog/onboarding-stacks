07139e22e7f3b81e92b95724e0fde0a472be31f2
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-02-13 10:10:15 +0100

    minor update

--- stack-mysql/terraform/aws/main.tf
+++ stack-mysql/terraform/aws/main.tf
@@ -8,6 +8,14 @@ module "rds" {
   cyorg     = var.cyorg
   #####################################
 
+  #. rds_engine_version: '8.0'
+  #+ The MySQL engine version to use.
+  rds_engine_version = "8.0"
+
+  #. rds_instance_class: 'db.t4g.micro'
+  #+ The instance type of the RDS instance.
+  rds_instance_class = "db.t4g.micro"
+
   #. rds_snapshot_identifier: ''
   #+ Create this database from a snapshot. This corresponds to the snapshot ID you'd find in the RDS console, e.g: rds:production-2015-06-26-06-05.
   rds_snapshot_identifier = "Value injected by StackForms"
