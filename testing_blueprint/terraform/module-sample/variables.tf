e333dd7fb702ba04461494fece97ef1b146483ef
admin admin <admin@cycloid>
2025-02-17 19:31:09 +0000

    Create Service Catalog: testing_blueprint from template: cycloid:terraform-sample

---
+++ testing_blueprint/terraform/module-sample/variables.tf
@@ -0,0 +1,7 @@
+variable "tshirt_size" {} # This is a variable example
+
+# Cycloid variables defined in ../provider.tf. And passed through ../main.tf.sample
+# usually used to properly tag cloud provider resources
+variable "env" {}
+variable "project" {}
+variable "organization" {}
